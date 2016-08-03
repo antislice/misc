d89f0b28b3cc4e0c8a1c2057682d059edc0344e7
Georgia Andrews <georgia@codeforamerica.org>
2016-08-02 19:29:21 -0700

    update & generalize

--- encode-decode.rb
+++ encode-decode.rb
@@ -1,10 +1,22 @@
 # helper script originally used to surface bad characters I found at the
 # beginning of the caltrain txt files
-string = File.read("google_transit_static_devs/#{ARGV.first}.txt")
+file_name = ARGV.first
+
+if file_name.nil?
+  puts 'please pass in a file'
+  exit 1
+end
+
+unless File.exist? file_name
+  puts 'could not find file'
+  exit
+end
+
+string = File.read(file_name)
 puts string.encoding
 string.encode!('UTF-8', undef: :replace, invalic: :replace, replace: '')
 # string.encode!('UTF-8')
 puts string.encoding
-File.open("google_transit_static_devs/#{ARGV.first}.txt", 'w') do |f|
+File.open(file_name, 'w') do |f|
   f.puts string
 end
